75c7a04682fc229962d09010e20799c03eab64e5
Joshua Carrafa <carrafa@gmail.com>
2016-01-18 14:52:36 -0500

    adds race start function/timer and status window

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,14 +4,35 @@ var port = 8080; //does this have to be hidden in .env?
 
 var socket = io.connect('localhost:' + port);
 
-var pos = 0; //starting point
+var pos = 0; // starting point
+var speed = 1; //speed.  starts at 1.  could change depending on how fast the game will be
 var id = Math.floor(Math.random() * 100); //random id for now.  will fill with database id later
 var randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16); //random color for now.  can let player choose color later.
+var countdown = 5;
 
 initData = {
   id: id,
   left: pos,
   color: randomColor
+}
+
+var raceStart = false;
+
+function startRace() {
+  var intervalID = window.setInterval(function() {
+    console.log(countdown);
+    $('#timer').text(countdown);
+    if (countdown === 0) {
+      $('#timer').text('go!');
+      raceStart = true;
+    }
+    if (countdown < 0) {
+      $('#timer').text('');
+      clearInterval(intervalID);
+      countdown = 5;
+    }
+    countdown--;
+  }, 1000)
 }
 
 function renderPlayer(data) { //renders player on screen
@@ -30,8 +51,8 @@ function initializePlayer(initData) { //initialization function.  used so every 
 
 function setKeyboardListener() { //listens for keyboard input
   $('body').on('keydown', 'input', function(e) {
-    if (sentenceChecker()) {;
-      pos = pos + 1;
+    if (raceStart && sentenceChecker()) {;
+      pos = pos + speed;
       data = {
         "left": pos + "px",
         "id": id
@@ -60,13 +81,17 @@ function updateAllPlayers(allPlayers) { // empties racetrack and updates with ne
 
 function globalListener() { // listens for socket messages
   socket.on('update all players', function(allPlayerData) {
-    console.log(allPlayerData);
     updateAllPlayers(allPlayerData);
   });
-  socket.on('user disconnected', function(id) {
+  socket.on('user disconnected', function(id, allPlayerData) {
     console.log(id);
     $('#', id).remove();
+    updateAllPlayers(allPlayerData);
   });
+  socket.on('start race', function() {
+    $('#status').append($('<li>').text('race tiiiime'));
+    startRace();
+  })
 };
 
 $(function() { // start 'er upppppp
