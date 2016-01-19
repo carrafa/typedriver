aaf2eeea5527786d07626a1909c880fa5a990e5f
Jeong Kim <joshkim83@outlook.com>
2016-01-19 18:06:21 -0500

    commiting finisher api in routes

--- routes/finishers.js
+++ routes/finishers.js
@@ -3,6 +3,38 @@ var express = require('express');
 var router = express.Router();
 var Finisher = require('../models/finisher');
 
+//GET ALL
+router.get('/', function(req, res){
+  Finisher.find({}, function(err, dbFinishers) {
+    res.json({finishers: dbFinishers});
+  })
+});
+
+// GET SINGLE RESOURCE
+router.get('/:id', function(req, res, next){
+  Finisher.findById(req.params.id, function(err, dbFinisher) {
+    if (err) { return next(err); }
+    if (!dbFinisher) { return next({status: 404, message: 'Not found'}); }
+    res.json(dbFinisher);
+  });
+});
+
+// POST
+router.post('/', function(req, res, next) {
+  console.log('creating!');
+  if (!req.body.finisher) { return next({status:422, message: 'Missing arguments' }); }
+  Finisher.create(req.body.finisher, function(err, finisher){
+    res.redirect('/');
+  })
+});
+
+// DELETE
+router.delete('/:id', function(req, res){
+  console.log("deleting");
+  Finisher.findByIdAndRemove(req.params.id, function(err){
+    res.status(204).end();
+  })
+});
 
 
 module.exports = router;
