6f8d3139a8844d4c696353610d0e336e4e926b39
Joshua Carrafa <carrafa@gmail.com>
2016-01-20 12:40:16 -0500

    heroku cors fix

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.use(bodyParser.urlencoded({
 var loadUser = require('./middleware/loadUser');
 app.use(loadUser);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // routes
 var users = require('./routes/users');
 app.use('/api/users', users);
