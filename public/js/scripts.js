eabd601ffd93b604aee2bd98904579fe5ddfffcd
Joshua Carrafa <carrafa@gmail.com>
2016-01-20 13:19:25 -0500

    another heroku sockets fix attempt

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,7 @@
 console.log('hello, i am scripts.js');
 
 // var socketUrl = 'localhost:8080'; //  this is for development
-var socketUrl = 'drywave.heroku.com:80'; //  this is for herokku
+var socketUrl = 'drywave.heroku.com:8080'; //  this is for herokku
 
 var socket = io.connect(socketUrl);
 
