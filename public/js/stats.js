f36b7ce4dd38969f7286c1676ea935122fe42323
Jeong Kim <joshkim83@outlook.com>
2016-01-21 17:03:34 -0500

    took out an obsolete function in client side stats js

--- public/js/stats.js
+++ public/js/stats.js
@@ -52,16 +52,6 @@ function getFinishers() {
   });
 };
 
-function getPlayersInRace(raceId) {
-  $.ajax({
-    url: '/api/finishers?search=' + raceId,
-    method: 'get',
-    success: function(data) {
-      return data
-    }
-  });
-}
-
 
 $(function() {
   getFinishers();
