03b5ee0235e5201c537730a7fbfde39ff762e870
Jeong Kim <joshkim83@outlook.com>
2016-01-21 12:52:16 -0500

    stats in public js has a commented out portion at the bottom

--- public/js/stats.js
+++ public/js/stats.js
@@ -73,3 +73,16 @@ $(function() {
   getFinishers();
 
 });
+
+
+// $('input#search-field').on('keyup', function(){
+//   var searchText = $(this).val();
+//   $.ajax({
+//     url: '/api/finishers?search=' + searchText,
+//     success: function(data){
+//       var finishers = data.finishers;
+//       var $list = $('#finisher-list');
+//       renderInfoList(finishers, $list)
+//     }
+//   })
+// })
